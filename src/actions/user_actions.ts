e230e933d31366e325e14d1453cdd21e29e6e126
Siddharth-Nema <siddharthnema2308@gmail.com>
2024-03-02 15:07:27 +0530

    prisma deleted

--- src/actions/user_actions.ts
+++ src/actions/user_actions.ts
@@ -1,21 +1,10 @@
 "use server";
 
-import { User } from "@prisma/client";
-import { db } from "~/server/db";
 import { courseData } from "./data";
-
-export const UpdateUser = (user: User) => {
-  return db.user.update({
-    where: { id: user.id },
-    data: {
-      ...user,
-    },
-  });
-};
 
 export const getCourseDetails = (formData: FormData) => {
   const courseId = formData.get("courseId");
   const course: { [key: string]: any } = courseData;
-  console.log(courseId, course[String(courseId)]);
+
   return course[String(courseId)];
 };
